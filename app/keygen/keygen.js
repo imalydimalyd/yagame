a2c04030986e29eb9a92e9ea6b93adcc86f2a3c3
imalydimalyd <thomas.gu@qq.com>
2026-03-04 15:27:06 +0800

    optimize key generation tool

--- app/keygen/keygen.js
+++ app/keygen/keygen.js
@@ -5,10 +5,26 @@ function generate() {
 	}
 	return key;
 }
-document.getElementById('send').addEventListener('click', function () {
-	printMessage({
-		user: '茉茉',
-		avatar: 'https://q1.qlogo.cn/g?b=qq&nk=3795740926&s=140',
-		content: generate(),
+const keyDisplayElement = document.getElementById('keydisplay');
+let key = '';
+function generateAndLoad() {
+	keyDisplayElement.style.transition = `none`;
+	keyDisplayElement.classList.add('hidden');
+	key = generate();
+	keyDisplayElement.innerText = key;
+	setTimeout(function () {
+		keyDisplayElement.style.transition = '';
+		keyDisplayElement.classList.remove('hidden');
+	}, 50);
+}
+document.getElementById('keygenerate').addEventListener('click', function () {
+	generateAndLoad();
+});
+document.getElementById('keycopy').addEventListener('click', function () {
+	navigator.clipboard.writeText(key).then(function () {
+		alert('复制成功！');
+	}, function (reason) {
+		alert('复制失败！原因：' + reason.toString());
 	});
 });
+generateAndLoad();
